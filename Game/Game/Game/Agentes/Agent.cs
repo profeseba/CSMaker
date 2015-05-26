29af481c70e63055739f28d5a84420e5c3b0e1ec
Sebastian <storresweb@gmail.com>
2015-05-26 12:38:11 -0400

    Reestructura del ARS

--- Game/Game/Game/Agentes/Agent.cs
+++ Game/Game/Game/Agentes/Agent.cs
@@ -116,7 +116,45 @@ namespace Game
             return nuevasAcciones;
         }
 
-        public abstract void Sensor(SpriteComponent sprite);
+        public void Sensor(SpriteComponent sprite) 
+        {
+            //asigna los estados
+            estados e = new estados();
+            e = Percepciones(sprite, this);
+            // acciones necesitan reglas
+            reglas r = new reglas();
+            //r.regla = new List<condiciones>();
+            //// condiciones
+            //condiciones cond1 = new condiciones(); // tecnicamente esto es una regla
+            //cond1.accion = "perseguir";
+            //cond1.condicion = new List<string>();
+            //cond1.condicion.Add("player_near"); // y esto son las condiciones de dicha regla
+            //condiciones cond2 = new condiciones();
+            //cond2.accion = "saltar";
+            //cond2.condicion = new List<string>();
+            //cond2.condicion.Add("block_is_near");
+            //condiciones cond3 = new condiciones();
+            //cond3.accion = "avanzar";
+            //cond3.condicion = new List<string>();
+            ////cond3.condicion.Add("player_no_near");
+            //cond3.condicion.Add("muro_no_near");
+            //condiciones cond4 = new condiciones();
+            //cond4.accion = "cambiar_direccion";
+            //cond4.condicion = new List<string>();
+            //cond4.condicion.Add("muro_is_near");
+            ////
+            //r.regla.Add(cond1);
+            //r.regla.Add(cond2);
+            //r.regla.Add(cond4);
+            //r.regla.Add(cond3);
+            ////
+            //////XML.Serialize(r,"reglas.dat");
+            r = XML.Deserialize<reglas>("reglas.dat");
+            acciones action = new acciones();
+            action = Regla(e, r);
+            //
+            Comportamiento(action);
+        }
 
         public abstract void Comportamiento(acciones a);
 
